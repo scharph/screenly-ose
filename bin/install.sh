157cd3b9fc07a64c6c381cca20120b38202550d8
Philipp Schartlmueller <Philipp.Schartlmueller@engel.at>
2020-09-21 17:53:26 +0200

    minimize install script

--- bin/install.sh
+++ bin/install.sh
@@ -5,119 +5,18 @@ BRANCH_VERSION=
 MANAGE_NETWORK=
 UPGRADE_SYSTEM=
 
-if [ -f .env ]; then
-  source .env
-fi
 
-while getopts ":w:b:n:s:" arg; do
-  case "${arg}" in
-    w)
-      WEB_UPGRADE=true
-      ;;
-    b)
-      BRANCH_VERSION=${OPTARG}
-      ;;
-    n)
-      MANAGE_NETWORK=${OPTARG}
-      ;;
-    s)
-      UPGRADE_SYSTEM=${OPTARG}
-      ;;
-  esac
-done
-
-if [ "$WEB_UPGRADE" = false ]; then
-
-  # Make sure the command is launched interactive.
-  if ! [ -t 0  ]; then
-    echo -e "Detected old installation command. Please use:\n$ bash <(curl -sL https://www.screenlyapp.com/install-ose.sh)"
-    exit 1
-  fi
-
-  # Set color of logo
-  tput setaf 4
-
-  cat << EOF
-       _____                           __         ____  _____ ______
-      / ___/_____________  ___  ____  / /_  __   / __ \/ ___// ____/
-      \__ \/ ___/ ___/ _ \/ _ \/ __ \/ / / / /  / / / /\__ \/ __/
-     ___/ / /__/ /  /  __/  __/ / / / / /_/ /  / /_/ /___/ / /___
-    /____/\___/_/   \___/\___/_/ /_/_/\__, /   \____//____/_____/
-                                     /____/
-EOF
-
-  # Reset color
-  tput sgr 0
-
-  echo -e "Screenly OSE requires a dedicated Raspberry Pi / SD card.\nYou will not be able to use the regular desktop environment once installed.\n"
-  read -p "Do you still want to continue? (y/N)" -n 1 -r -s INSTALL
-  if [ "$INSTALL" != 'y' ]; then
-    echo
-    exit 1
-  fi
-
-  if [ -z "${BRANCH}" ]; then
-    echo && read -p "Would you like to use the experimental branch? It contains the last major changes, such as the new browser and migrating to Docker (y/N)" -n 1 -r -s EXP && echo
-    if [ "$EXP" != 'y'  ]; then
-      echo && read -p "Would you like to use the development (master) branch? You will get the latest features, but things may break. (y/N)" -n 1 -r -s DEV && echo
-      if [ "$DEV" != 'y'  ]; then
-        export DOCKER_TAG="production"
-        BRANCH="production"
-      else
-        export DOCKER_TAG="latest"
-        BRANCH="master"
-      fi
-    else
-      export DOCKER_TAG="experimental"
-      BRANCH="experimental"
-    fi
-  fi
-
-  echo && read -p "Do you want Screenly to manage your network? This is recommended for most users because this adds features to manage your network. (Y/n)" -n 1 -r -s NETWORK && echo
-
-  echo && read -p "Would you like to perform a full system upgrade as well? (y/N)" -n 1 -r -s UPGRADE && echo
-  if [ "$UPGRADE" != 'y' ]; then
-    EXTRA_ARGS="--skip-tags enable-ssl,system-upgrade"
-  else
-    EXTRA_ARGS="--skip-tags enable-ssl"
-  fi
-
-elif [ "$WEB_UPGRADE" = true ]; then
-  if [ -z "${BRANCH}" ]; then
-    if [ "$BRANCH_VERSION" = "latest" ]; then
-      export DOCKER_TAG="latest"
-      BRANCH="master"
-    elif [ "$BRANCH_VERSION" = "production" ]; then
-      export DOCKER_TAG="production"
-      BRANCH="production"
-    else
-      echo -e "Invalid -b parameter."
-      exit 1
-    fi
-  fi
-
-  if [ "$MANAGE_NETWORK" = false ]; then
-    NETWORK="y"
-  elif [ "$MANAGE_NETWORK" = true ]; then
-    NETWORK="n"
-  else
-    echo -e "Invalid -n parameter."
-    exit 1
-  fi
-
-  if [ "$UPGRADE_SYSTEM" = false ]; then
-    EXTRA_ARGS="--skip-tags enable-ssl,system-upgrade"
-  elif [ "$UPGRADE_SYSTEM" = true ]; then
-    EXTRA_ARGS="--skip-tags enable-ssl"
-  else
-    echo -e "Invalid -s parameter."
-    exit 1
-  fi
-
-else
-  echo -e "Invalid -w parameter."
+echo -e "Screenly OSE requires a dedicated Raspberry Pi / SD card.\nYou will not be able to use the regular desktop environment once installed.\n"
+read -p "Do you still want to continue? (y/N)" -n 1 -r -s INSTALL
+if [ "$INSTALL" != 'y' ]; then
+  echo
   exit 1
 fi
+
+export DOCKER_TAG="production"
+BRANCH="production"
+    
+EXTRA_ARGS="--skip-tags enable-ssl"
 
 if grep -qF "Raspberry Pi 3" /proc/device-tree/model; then
   export DEVICE_TYPE="pi3"
@@ -127,15 +26,7 @@ else
   export DEVICE_TYPE="pi1"
 fi
 
-if [ -z "${REPOSITORY}" ]; then
-  if [ "$WEB_UPGRADE" = false ]; then
-    set -x
-    REPOSITORY=${1:-https://github.com/scharph/screenly-ose.git}
-  else
-    set -e
-    REPOSITORY=https://github.com/scharph/screenly-ose.git
-  fi
-fi
+REPOSITORY=https://github.com/scharph/screenly-ose.git
 
 sudo mkdir -p /etc/ansible
 echo -e "[local]\nlocalhost ansible_connection=local" | sudo tee /etc/ansible/hosts > /dev/null
@@ -152,13 +43,8 @@ sudo apt-get purge -y python-setuptools python-pip python-pyasn1
 sudo apt-get install -y python-dev git-core libffi-dev libssl-dev
 curl -s https://bootstrap.pypa.io/get-pip.py | sudo python
 
-if [ "$NETWORK" == 'y' ]; then
-  export MANAGE_NETWORK=true
-  sudo apt-get install -y network-manager
-else
-  export MANAGE_NETWORK=false
-fi
 
+export MANAGE_NETWORK=false
 sudo pip install ansible==2.8.2
 
 sudo -u pi ansible localhost -m git -a "repo=$REPOSITORY dest=/home/pi/screenly version=$BRANCH"
@@ -176,22 +62,6 @@ sudo find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en*' ! -name 'de*' 
 
 cd /home/pi/screenly && git rev-parse HEAD > /home/pi/.screenly/latest_screenly_sha
 sudo chown -R pi:pi /home/pi
-
-# Need a password for commands with sudo
-if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "production" ]; then
-  sudo rm -f /etc/sudoers.d/010_pi-nopasswd
-else
-  # Temporarily necessary because web upgrade only for the master branch
-  echo "pi ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/010_pi-nopasswd > /dev/null
-  sudo chmod 0440 /etc/sudoers.d/010_pi-nopasswd
-fi
-
-# Setup a new pi password
-if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "production" ] && [ "$WEB_UPGRADE" = false ]; then
-  set +e
-  passwd
-  set -e
-fi
 
 echo -e "Screenly version: $(git rev-parse --abbrev-ref HEAD)@$(git rev-parse --short HEAD)\n$(lsb_release -a)" > ~/version.md
 
